d39aa24f7baa1cb4d9f1a99f067710d804333adb
Carl Howells <chowells79@gmail.com>
2024-01-12 03:34:46 -0800

    pandocify

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,21 @@
+{-# Language OverloadedStrings #-}
 module Main where
 
+import Hakyll
+
+config :: Configuration
+config = defaultConfiguration
+    { destinationDirectory = "docs"
+    , providerDirectory = "content"
+    }
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = hakyllWith config $ do
+    match "*.md" $ do
+        route $ setExtension ".html"
+        compile $ pandocCompiler >>=
+            loadAndApplyTemplate "templates/default.html" defaultContext >>=
+            relativizeUrls
+
+    match "templates/*" $ compile templateCompiler
+    match "CNAME" $ route idRoute >> compile copyFileCompiler
