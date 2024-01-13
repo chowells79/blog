e36dd72bcb5aa2a272a7f14dcca007706ac6328c
Carl Howells <chowells79@gmail.com>
2024-01-12 17:51:14 -0800

    minor refactoring, move small static files to filesystem

--- app/Main.hs
+++ app/Main.hs
@@ -6,27 +6,31 @@ import Hakyll
 config :: Configuration
 config = defaultConfiguration
     { destinationDirectory = "docs"
+    , ignoreFile = const False
     , providerDirectory = "content"
     }
+
+staticFiles :: Pattern
+staticFiles = foldr1 (.||.) [image, font, stylesheet, javascript, github]
+  where
+    font = "fonts/**" .&&. ("**.ttf" .||. "**.otf" .||. "**.woff" .||. "**.woff2")
+    stylesheet = "css/**" .&&. "**.css"
+    javascript = "js/**" .&&. "**.js"
+    image = "**.gif" .||. "**.jpg" .||. "**.jpeg" .||. "**.png" .||. "favicon.ico"
+    github = ".nojekyll" .||. "CNAME"
+
 
 main :: IO ()
 main = hakyllWith config $ do
     match "*.md" $ do
         route $ setExtension ".html"
-        compile $ pandocCompiler >>=
-            loadAndApplyTemplate "templates/default.html" defaultContext >>=
-            relativizeUrls
+        compile $ do
+            c <- pandocCompiler
+            fullPage <- loadAndApplyTemplate "templates/default.html" defaultContext c
+            relativizeUrls fullPage
 
     match "templates/*" $ compile templateCompiler
 
-    match ("*.png" .||. "favicon.ico") $ do
+    match staticFiles $ do
         route idRoute
         compile copyFileCompiler
-
-    create [".nojekyll"] $ do
-        route idRoute
-        compile $ makeItem ("" :: String)
-
-    create ["CNAME"] $ do
-        route idRoute
-        compile $ makeItem ("blog.adjoint.us" :: String)
