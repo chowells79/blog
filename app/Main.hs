eba3049145f245c07600550a343083f8f4f70482
Carl Howells <chowells79@gmail.com>
2024-02-25 00:45:54 -0800

    Handle option parsing manually so that additional arguments can be supported

--- app/Main.hs
+++ app/Main.hs
@@ -1,90 +1,28 @@
-{-# Language OverloadedStrings #-}
 module Main where
 
-import Data.Foldable (forM_)
-import Data.List (inits, intercalate, nub)
-import Data.List.Split (splitOn)
+import Site (fullSite)
+
 import GHC.Stack (HasCallStack)
 
-import Hakyll
-import Hakyll.SkylightingCss (skylightingCssCompiler)
+import Hakyll.Main (Options, hakyllWithArgs, optionParser)
+import Hakyll.Core.Configuration (defaultConfiguration, providerDirectory)
 
-staticFiles :: HasCallStack => Rules ()
-staticFiles = do
-    -- just copy these over verbatim
-    match common $ do
-        route idRoute
-        compile copyFileCompiler
+import Options.Applicative
 
-    -- If a browser falls back to looking for favicon.ico, it always
-    -- looks for it at the top level
-    match "img/favicon.ico" $ do
-        route $ constRoute "favicon.ico"
-        compile copyFileCompiler
+data MyOpts = Echo String | Hakyll Options deriving (Show)
+
+myOptsParser :: Parser Options -> Parser MyOpts
+myOptsParser opt = Echo <$> e <|> Hakyll <$> opt
   where
-    common = images .||. fonts .||. scripts
-    fonts = "fonts/**" .&&. ("**.woff" .||. "**.woff2")
-    scripts = "js/**" .&&. ("**.js" .||. "**.js.map")
-    images = "img/**" .&&. ("**.gif" .||. "**.jpg" .||. "**.png")
-
-
--- Generate files github treats specially at the top level
-specialFiles :: HasCallStack => Rules ()
-specialFiles = do
-    match "CNAME" $ do
-        route idRoute
-        compile copyFileCompiler
-
-    let tpl = "templates/default.html"
-        applyDefault = loadAndApplyTemplate tpl defaultContext
-
-    match ("index.md" .||. "404.md") $ do
-        route $ setExtension "html"
-        compile $ pandocCompiler >>= applyDefault
-
-
-buildStylesheets :: HasCallStack => Rules ()
-buildStylesheets = do
-    -- compress external stylesheets to deploy directory
-    match ("css/**" .&&. css) $ do
-        route idRoute
-        compile compressCssCompiler
-
-    -- generate CSS from Skylighting files
-    match ("css/**" .&&. sky) $ do
-        route $ setExtension "css"
-        compile $ do
-            style <- itemBody <$> getResourceString
-            skylightingCssCompiler style
-
-    -- create concatenated CSS from subdirectory contents.
-    let pat = "css/**/*" .&&. both
-    dep <- makePatternDependency pat
-    rulesExtraDependencies [dep] $ do
-        idents <- getMatches pat
-        let extractParent = maybe
-                            (error "impossible stylesheet subdirectory mismatch")
-                            (foldr const (error "impossible stylesheet subdirectory match"))
-            multiply = map (intercalate "/") . drop 2 . inits . splitOn "/"
-            dirs = nub . concatMap (multiply . extractParent . capture "**/*") $ idents
-        forM_ dirs $ \dirName -> do
-            let dirPattern = fromGlob (dirName ++ "/*") .&&. both
-            create [ fromFilePath $ dirName ++ ".css" ] $ do
-                route idRoute
-                compile $ do
-                    cssContents <- map itemBody <$> loadAll dirPattern
-                    makeItem . compressCss . concat $ cssContents
-  where
-    css = "**.css"
-    sky = "**.skylighting"
-    both = css .||. sky
+    e = strOption (long "echo" <> help "Provide a value to echo")
 
 main :: HasCallStack => IO ()
 main = do
     let config = defaultConfiguration { providerDirectory = "content"}
-    hakyllWith config $ do
-        match "templates/**" $ compile templateCompiler
-
-        specialFiles
-        staticFiles
-        buildStylesheets
+        parser = helper <*> myOptsParser (optionParser config)
+        parserInfo = info parser (fullDesc <> progDesc desc)
+        desc = "blog - A static site compiler"
+    opts <- execParser parserInfo
+    case opts of
+        Echo e -> putStrLn e
+        Hakyll o -> hakyllWithArgs config o fullSite
---
+++ app/Site.hs
@@ -0,0 +1,93 @@
+{-# Language OverloadedStrings #-}
+module Site
+  ( fullSite
+  )
+where
+
+import Data.Foldable (forM_)
+import Data.List (inits, intercalate, nub)
+import Data.List.Split (splitOn)
+import GHC.Stack (HasCallStack)
+
+import Hakyll
+import Hakyll.SkylightingCss (skylightingCssCompiler)
+
+
+staticFiles :: HasCallStack => Rules ()
+staticFiles = do
+    -- just copy these over verbatim
+    match common $ do
+        route idRoute
+        compile copyFileCompiler
+
+    -- If a browser falls back to looking for favicon.ico, it always
+    -- looks for it at the top level
+    match "img/favicon.ico" $ do
+        route $ constRoute "favicon.ico"
+        compile copyFileCompiler
+  where
+    common = images .||. fonts .||. scripts
+    fonts = "fonts/**" .&&. ("**.woff" .||. "**.woff2")
+    scripts = "js/**" .&&. ("**.js" .||. "**.js.map")
+    images = "img/**" .&&. ("**.gif" .||. "**.jpg" .||. "**.png")
+
+
+-- Generate files github treats specially at the top level
+specialFiles :: HasCallStack => Rules ()
+specialFiles = do
+    match "CNAME" $ do
+        route idRoute
+        compile copyFileCompiler
+
+    let tpl = "templates/default.html"
+        applyDefault = loadAndApplyTemplate tpl defaultContext
+
+    match ("index.md" .||. "404.md") $ do
+        route $ setExtension "html"
+        compile $ pandocCompiler >>= applyDefault
+
+
+buildStylesheets :: HasCallStack => Rules ()
+buildStylesheets = do
+    -- compress external stylesheets to deploy directory
+    match ("css/**" .&&. css) $ do
+        route idRoute
+        compile compressCssCompiler
+
+    -- generate CSS from Skylighting files
+    match ("css/**" .&&. sky) $ do
+        route $ setExtension "css"
+        compile $ do
+            style <- itemBody <$> getResourceString
+            skylightingCssCompiler style
+
+    -- create concatenated CSS from subdirectory contents.
+    let pat = "css/**/*" .&&. both
+    dep <- makePatternDependency pat
+    rulesExtraDependencies [dep] $ do
+        idents <- getMatches pat
+        let extractParent = maybe
+                            (error "impossible stylesheet subdirectory mismatch")
+                            (foldr const (error "impossible stylesheet subdirectory match"))
+            multiply = map (intercalate "/") . drop 2 . inits . splitOn "/"
+            dirs = nub . concatMap (multiply . extractParent . capture "**/*") $ idents
+        forM_ dirs $ \dirName -> do
+            let dirPattern = fromGlob (dirName ++ "/*") .&&. both
+            create [ fromFilePath $ dirName ++ ".css" ] $ do
+                route idRoute
+                compile $ do
+                    cssContents <- map itemBody <$> loadAll dirPattern
+                    makeItem . compressCss . concat $ cssContents
+  where
+    css = "**.css"
+    sky = "**.skylighting"
+    both = css .||. sky
+
+
+fullSite :: HasCallStack => Rules ()
+fullSite = do
+    match "templates/**" $ compile templateCompiler
+
+    specialFiles
+    staticFiles
+    buildStylesheets
