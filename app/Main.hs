339f73d1edd212396f0a39d83229f95d6b83a575
Carl Howells <chowells79@gmail.com>
2024-02-25 21:46:06 -0800

    Refactor logic a bit

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,7 @@
 module Main where
 
 import Site (fullSite)
-import Parser (BlogOptions(..), BlogCommand(..), getOptions)
+import Parser (BlogCommand(..), blogOptions, getOptions)
 
 import Control.Monad (when)
 
@@ -11,17 +11,19 @@ import Hakyll.Main (Options(..), hakyllWithArgs)
 import Hakyll.Core.Configuration (defaultConfiguration, providerDirectory)
 
 
+runCommand :: HasCallStack => Bool -> BlogCommand -> IO ()
+runCommand verbose c = case c of
+    Echo str -> do
+        when verbose $ putStrLn "Doing an echo"
+        putStrLn str
+    Hakyll command -> do
+        let config = defaultConfiguration
+                     { providerDirectory = "content"}
+            hOpts = Options verbose command
+        hakyllWithArgs config hOpts fullSite
+
 
 main :: HasCallStack => IO ()
 main = do
     opts <- getOptions
-    case opts of
-        BlogOptions verbose c -> case c of
-            Echo str -> do
-                when verbose $ putStrLn "Doing an echo"
-                putStrLn str
-            Hakyll command -> do
-                let config = defaultConfiguration
-                             { providerDirectory = "content"}
-                    hOpts = Options verbose command
-                hakyllWithArgs config hOpts fullSite
+    blogOptions runCommand opts
--- app/Parser.hs
+++ app/Parser.hs
@@ -1,6 +1,7 @@
 module Parser
   ( BlogOptions(..)
   , BlogCommand(..)
+  , blogOptions
   , getOptions
   ) where
 
@@ -11,6 +12,8 @@ import GHC.Stack (HasCallStack)
 import Hakyll.Main (Command, defaultCommands)
 import Hakyll.Core.Configuration (defaultConfiguration)
 
+import Control.Lens hiding (argument)
+
 import Options.Applicative
 
 
@@ -18,15 +21,23 @@ data BlogOptions = BlogOptions Bool BlogCommand deriving Show
 data BlogCommand = Echo String | Hakyll Command deriving Show
 
 
+-- fold for BlogOptions
+blogOptions :: (Bool -> BlogCommand -> r) -> BlogOptions -> r
+blogOptions f (BlogOptions v c) = f v c
+
+
+---------------------------------------------------------------------
+-- logic for remaining definitions adapted from Hakyll definitions --
+---------------------------------------------------------------------
+
+
 commandParser :: HasCallStack => Parser BlogCommand
 commandParser = subparser $ foldMap toCommand commands
   where
     toCommand (a, b, c) = command a $ info (helper <*> b) c
     commands = blogCommands ++ hakyllCommands
-    hakyllCommands =
-        [ (a, Hakyll <$> b, c)
-        | (a, b, c) <- defaultCommands defaultConfiguration
-        ]
+    hakyllCommands = over (traverse . _2) (fmap Hakyll) $
+        defaultCommands defaultConfiguration
     blogCommands =
         [ ( "echo"
           , Echo . intercalate " " <$> many (argument str mempty)
@@ -39,7 +50,7 @@ optionParser :: HasCallStack => Parser BlogOptions
 optionParser = BlogOptions <$> verboseParser <*> commandParser
   where
     verboseParser = switch mods
-    mods = (long "verbose" <> short 'v' <> help "Run in verbose mode")
+    mods = long "verbose" <> short 'v' <> help "Run in verbose mode"
 
 
 getOptions :: HasCallStack => IO BlogOptions
